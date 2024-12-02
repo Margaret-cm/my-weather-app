9d47f830cb6f63d83e139b4e1f13b2ec09e9f7b3
Margaret-cm <mukanganwamc@gmail.com>
2024-12-02 11:46:55 +0200

    Added search engine functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateTemperature(response) {
+  let currentTemperatureValue = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  currentTemperatureValue.innerHTML = Math.round(temperature);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "024aa03t44f40e2f16f8abod4036b3b9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateTemperature);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearch);
+
+searchCity("Harare");
