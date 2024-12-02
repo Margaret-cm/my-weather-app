6651026f6be9c0433f676b1c15a98c8bd3765786
Margaret-cm <mukanganwamc@gmail.com>
2024-12-02 14:14:43 +0200

    Updated day and time using API data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ function updateTemperature(response) {
   let currentTemperatureValue = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
+  let timeElement = document.querySelector("#time");
+  let currentDate = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
   currentTemperatureValue.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(currentDate);
+}
+
+function formatDate(currentDate) {
+  let hour = currentDate.getHours();
+  let minutes = currentDate.getMinutes().toString().padStart(2, "0");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+
+  // if (minutes < 10) {
+  // minutes = `0${minutes}`;
+  //}
+  return `${day}, ${hour}:${minutes}`;
 }
 
 function handleSearch(event) {
